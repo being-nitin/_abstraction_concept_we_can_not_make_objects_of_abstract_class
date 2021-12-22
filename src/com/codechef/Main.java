f1d55375ba54b0d03fd2ef5da1c72409750051af
Nitin <nsinghboss02@gmail.com>
2021-12-22 18:07:40 +0530

    concept added

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	/*
 	we can not make objects of abstract class:
+	if we make objects of abstract class i'e
+	Bank b1 = new Bank
+	it will show an error and compiler will
+	ask you to override the method in it too
 	 */
         Scanner sc = new Scanner(System.in);
         Sbi s1 = new Sbi();
         s1.B = sc.nextInt();
-        System.out.println(s1.transaction(););
+        s1.transaction();
 
     }
 }
